af80aca4ceabff4e78b43d88206f2aae1af5964c
James Michael DuPont <jamesmikedupont+build@gmail.com>
2017-08-12 07:43:51 -0400

    code cleanup

--- tests/pattern_class_test.py
+++ tests/pattern_class_test.py
@@ -1,96 +1,104 @@
 import sys
-
-sys.path.append("/home/mdupont/experiments/astunparse/lib/") # my hack for not installing the astunparse, just pip install it.
-
-import astunparse
 import pprint
-
-from ast import NodeTransformer,Str, Name, Param,  Module, FunctionDef, arguments, Return, keyword, Load, ClassDef, Assign, Store 
-
+from ast import NodeTransformer, Str, Name, Param
+from ast import Module, FunctionDef, arguments, Return, keyword
+from ast import Load, ClassDef, Assign, Store
 import ast
 import inspect
+
+# my hack for not installing the astunparse, just pip install it.
+sys.path.append("/home/mdupont/experiments/astunparse/lib/")
+
+import astunparse
+
 
 def extract_ast(thing):
     """
     internal step, takes the thing and extracts ast
     """
-    src =inspect.getsource(thing)
+    src = inspect.getsource(thing)
     return ast.parse(src)
 
+
 class ReplaceObjects(NodeTransformer):
-    def __init__(self,rep):
-        self.rep =rep
-    
+    def __init__(self, rep):
+        self.rep = rep
+
     def visit_Call(self, node):
         offset = 0
         for x in node.keywords:
-            if isinstance(x.value,Str):
+            if isinstance(x.value, Str):
                 sv = x.value.s
                 if sv in self.rep:
                     v = self.rep[sv]
                     key = node.keywords[offset].arg
-                    c= create_load_name_dump(key,v)
+                    c = create_load_name_dump(key, v)
                     node.keywords[offset] = c
             offset = offset + 1
         return self.generic_visit(node)
-    
+
+
 def do_replacements_objects(aast, **replacements):
     r = ReplaceObjects(replacements)
-    nast  = r.visit(aast)
+    nast = r.visit(aast)
     return nast
-    
-def define_pattern2(methodname,thing,**kw):
+
+
+def define_pattern2(methodname, thing, **kw):
     args = []
-    newkw = {} #replace the string with variables
-    
+    newkw = {}  # replace the string with variables
+
     # create a set of loads for the variables
     for k in kw:
         v = kw[k]
-        #l = create_load_name(v)
         p = create_param_name(v)
         args.append(p)
-        newkw[k]=v
-                    
+        newkw[k] = v
+
     aast = extract_ast(thing)
     ad = astunparse.dump(aast)
     newast = ast.parse(ad)
     newast2 = newast
-    body = do_replacements_objects(newast2,**newkw)
-    
-    skip = body.body[0].value # why do we need this?
+    body = do_replacements_objects(newast2, **newkw)
+
+    skip = body.body[0].value  # why do we need this?
     # we traverse the tree and remove the extra
     # Module(body=[Expr(value=Call(
-    
-    return define_replace_method(methodname,args, skip)
 
-def create_load_name_dump(key,_id):
+    return define_replace_method(methodname, args, skip)
+
+
+def create_load_name_dump(key, _id):
     return [
-            keyword(
-                arg=key,
-                value=Name(id=_id,ctx=Load()))
-    ]        
+        keyword(
+            arg=key,
+            value=Name(id=_id, ctx=Load()))
+    ]
+
 
 def create_param_name(id):
     "creates a named parameter"
     return Name(
         id=id,
         ctx=Param())
-                        
-def define_replace_method(methodname,args, body):
+
+
+def define_replace_method(methodname, args, body):
     """
-    This generates an ast for a generic function that takes a list parameters and returns a generic body
-    used to construct something like this 
+    This generates an ast for a generic function that takes a list parameters
+    and returns a generic body
+    used to construct something like this
 
         def <methodname>(<arges>):
             return Module(body=<body>)
 
-    The args parameters are like this : 
+    The args parameters are like this :
         [ Name(id='<argname>', ctx=Param()), .... ]
     """
-    
-    return Module( # top module
+
+    return Module(  # top module
         body=[
-            FunctionDef( # new function
+            FunctionDef(  # new function
                 name=methodname,
                 args=arguments(
                     args=args,
@@ -101,38 +109,41 @@ def define_replace_method(methodname,args, body):
                     Return(value=body)
                 ],
                 decorator_list=[]
-            ) # module2
+            )  # module2
         ]
-    )# module
+    )  # module
 
 
-#### begin of testing
+# begin of testing
 
 class MetaClass(type):
     def __init__(cls, name, bases, nmspc):
         super(MetaClass, cls).__init__(name, bases, nmspc)
 
+
 class BaseClass(object):
     pass
 
-class ClassWithMetaclass(BaseClass) :
-     __metaclass__ = MetaClass
+
+class ClassWithMetaclass(BaseClass):
+    __metaclass__ = MetaClass
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
 
-    newmethod =define_pattern2("generate_metaclass",
-                    ClassWithMetaclass,
-                    ClassWithMetaclass = 'NewClassName',
-                    BaseClass = 'NewBaseClassName',
-                    MetaClass = 'MetaClassVariable') # replace with parameter variable
+    newmethod = define_pattern2(
+        "generate_metaclass",
+        ClassWithMetaclass,
+        ClassWithMetaclass='NewClassName',
+        BaseClass='NewBaseClassName',
+        MetaClass='MetaClassVariable')
 
-    #print(astunparse.dump(newmethod))
+    # print(astunparse.dump(newmethod))
     code = astunparse.unparse(newmethod)
-    print (code)
+    print(code)
 
-    c = compile(code,'<string>','exec')
+    c = compile(code, '<string>', 'exec')
     exec(c)
-    
-    mc = generate_metaclass("somemeta","somebase","someclass")
+
+    mc = generate_metaclass("somemeta", "somebase", "someclass")
     print(astunparse.unparse(mc))
