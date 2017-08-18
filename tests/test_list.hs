95d468e16df441a1754fa66aaf3b99f9f0ed30ef
James Michael DuPont <jamesmikedupont+build@gmail.com>
2017-08-17 21:19:21 -0400

    using types not strings

--- tests/test_list.hs
+++ tests/test_list.hs
@@ -1,5 +1,31 @@
 
-data Tree = SetStr String String
+data Tree = SetStr Tree Tree
+  -- constants
+  | AList
+  | Arg
+  | Args
+  | Arguments
+  | Body
+  | Call
+  | Ctx
+  | DecoratorList
+  | Defaults
+  | Elements
+  | ExampleEmptyFunction
+  | Func
+  | FunctionDef
+  | KeyWordArg
+  | KeyWordArgs
+  | KeyWords
+  | Ld
+  | Module
+  | Name
+  | None 
+  | Pass
+  | StarArgs
+  | Value
+  | VarArg
+  ---
   | Alist4 Tree Tree Tree Tree
   | Alist1 Tree
   | Attribute_body_type_list Tree
@@ -15,12 +41,13 @@ data Tree = SetStr String String
   | Record_type_body Tree
   | Record_type_ctx_elts Tree Tree
   | Record_type_value Tree
-  | Setrec String Tree
+  | Setrec Tree Tree
   | Alist0
   | Record_type_Call Tree
-  | NilAttr String
-  | SetAttr String Tree
-  | Node Int Tree Tree deriving Show
+  | NilAttr Tree
+  | SetAttr Tree Tree
+  | Node Int Tree Tree
+          deriving Show
 
 setstr (k, v) = SetStr k v
 nilattr x = NilAttr x
@@ -31,9 +58,6 @@ setattr (x,y) = SetAttr x y
 tupleToList :: [(a,a)] -> [a]
 tupleToList ((a,b):xs) = a : b : tupleToList xs
 tupleToList _          = []
-
---alist_4 :: (a, b, c, d) -> [a]
---alist_4 (a,b,c,d) = ("alist_4", [a,b,c,d])
 
 main :: IO()
 main = do 
@@ -105,75 +129,95 @@ record_type_value_42 x = record_type_value x
 record_type_body x = Record_type_body x
 record_type_body_43 x = record_type_body x
 
+
+a_list  = AList
+arg = Arg
+args = Args
+arguments  = Arguments
+body = Body
+ctx  = Ctx
+decorator_list = DecoratorList
+defaults = Defaults
+elts = Elements
+example_empty_function = ExampleEmptyFunction
+func = Func
+keywords = KeyWords
+kwarg = KeyWordArg
+kwargs = KeyWordArgs
+name = Name
+starargs =StarArgs
+value  = Value
+vararg = VarArg
+ 
 eval_module= record_type_body_43(arecord(
-    setattr("body", attribute_body_type_list_1 (alist_1(
+    setattr(body, attribute_body_type_list_1 (alist_1(
      achain(record_type_value_42(arecord(
-        setrec("value", record_type_Call_41(arecord(
-          setrec("Call", record_type_args_func_keywords_kwargs_starargs_40(arecord(
-            setattr("keywords", attribute_keywords_type_list_2 (alist_1(
+        setrec(value, record_type_Call_41(arecord(
+          setrec(Call, record_type_args_func_keywords_kwargs_starargs_40(arecord(
+            setattr(keywords, attribute_keywords_type_list_2 (alist_1(
              achain(record_type_arg_value_38(arecord(
-                setrec("value", record_type_a_list_37(arecord(
-                  setrec("a_list", record_type_ctx_elts_36(arecord(
-                    setattr("elts", attribute_elts_type_list_3 (alist_1(
+                setrec(value, record_type_a_list_37(arecord(
+                  setrec(a_list, record_type_ctx_elts_36(arecord(
+                    setattr(elts, attribute_elts_type_list_3 (alist_1(
                      achain(record_type_Call_35(arecord(
-                        setrec("Call", record_type_args_func_keywords_kwargs_starargs_34(arecord(
-                          setattr("keywords", attribute_keywords_type_list_4 (alist_4(
+                        setrec(Call, record_type_args_func_keywords_kwargs_starargs_34(arecord(
+                          setattr(keywords, attribute_keywords_type_list_4 (alist_4(
                            achain(record_type_arg_value_5(arecord(
-                              setstr("value","example_empty_function"),
-                              setstr("arg","name")))),
+                              setstr(value,example_empty_function),
+                              setstr(arg,name)))),
                            achain(record_type_arg_value_20(arecord(
-                              setrec("value", record_type_Call_19(arecord(
-                                setrec("Call", record_type_args_func_keywords_kwargs_starargs_18(arecord(
-                                  setattr("keywords", attribute_keywords_type_list_6 (alist_4(
+                              setrec(value, record_type_Call_19(arecord(
+                                setrec(Call, record_type_args_func_keywords_kwargs_starargs_18(arecord(
+                                  setattr(keywords, attribute_keywords_type_list_6 (alist_4(
                                    achain(record_type_arg_value_10(arecord(
-                                      setrec("value", record_type_a_list_9(arecord(
-                                        setrec("a_list", record_type_ctx_elts_8(arecord(
-                                          setattr("elts", attribute_elts_type_list_7 (alist_0())),
-                                          setstr("ctx","Ld"))))))),
-                                      setstr("arg","args")))),
+                                      setrec(value, record_type_a_list_9(arecord(
+                                        setrec(a_list, record_type_ctx_elts_8(arecord(
+                                          setattr(elts, attribute_elts_type_list_7 (alist_0())),
+                                          setstr(ctx,Ld))))))),
+                                      setstr(arg,args)))),
                                    achain(record_type_arg_value_11(arecord(
-                                      setstr("value","None"),
-                                      setstr("arg","vararg")))),
+                                      setstr(value,None),
+                                      setstr(arg,vararg)))),
                                    achain(record_type_arg_value_12(arecord(
-                                      setstr("value","None"),
-                                      setstr("arg","kwarg")))),
+                                      setstr(value,None),
+                                      setstr(arg,kwarg)))),
                                    achain(record_type_arg_value_16(arecord(
-                                      setrec("value", record_type_a_list_15(arecord(
-                                        setrec("a_list", record_type_ctx_elts_14(arecord(
-                                          setattr("elts", attribute_elts_type_list_13 (alist_0())),
-                                          setstr("ctx","Ld"))))))),
-                                      setstr("arg","defaults"))))))),
-                                  nilattr("kwargs"),
-                                  setattr("args", attribute_args_type_list_17 (alist_0())),
-                                  setstr("func","arguments"),
-                                  nilattr("starargs"))))))),
-                              setstr("arg","args")))),
+                                      setrec(value, record_type_a_list_15(arecord(
+                                        setrec(a_list, record_type_ctx_elts_14(arecord(
+                                          setattr(elts, attribute_elts_type_list_13 (alist_0())),
+                                          setstr(ctx,Ld))))))),
+                                      setstr(arg,defaults))))))),
+                                  nilattr(kwargs),
+                                  setattr(args, attribute_args_type_list_17 (alist_0())),
+                                  setstr(func,arguments),
+                                  nilattr(starargs))))))),
+                              setstr(arg,args)))),
                            achain(record_type_arg_value_28(arecord(
-                              setrec("value", record_type_a_list_27(arecord(
-                                setrec("a_list", record_type_ctx_elts_26(arecord(
-                                  setattr("elts", attribute_elts_type_list_21 (alist_1(
+                              setrec(value, record_type_a_list_27(arecord(
+                                setrec(a_list, record_type_ctx_elts_26(arecord(
+                                  setattr(elts, attribute_elts_type_list_21 (alist_1(
                                    achain(record_type_Call_25(arecord(
-                                      setrec("Call", record_type_args_func_keywords_kwargs_starargs_24(arecord(
-                                        setattr("keywords", attribute_keywords_type_list_22 (alist_0())),
-                                        nilattr("kwargs"),
-                                        setattr("args", attribute_args_type_list_23 (alist_0())),
-                                        setstr("func","Pass"),
-                                        nilattr("starargs")))))))))),
-                                  setstr("ctx","Ld"))))))),
-                              setstr("arg","body")))),
+                                      setrec(Call, record_type_args_func_keywords_kwargs_starargs_24(arecord(
+                                        setattr(keywords, attribute_keywords_type_list_22 (alist_0())),
+                                        nilattr(kwargs),
+                                        setattr(args, attribute_args_type_list_23 (alist_0())),
+                                        setstr(func,Pass),
+                                        nilattr(starargs)))))))))),
+                                  setstr(ctx,Ld))))))),
+                              setstr(arg,body)))),
                            achain(record_type_arg_value_32(arecord(
-                              setrec("value", record_type_a_list_31(arecord(
-                                setrec("a_list", record_type_ctx_elts_30(arecord(
-                                  setattr("elts", attribute_elts_type_list_29 (alist_0())),
-                                  setstr("ctx","Ld"))))))),
-                              setstr("arg","decorator_list"))))))),
-                          nilattr("kwargs"),
-                          setattr("args", attribute_args_type_list_33 (alist_0())),
-                          setstr("func","FunctionDef"),
-                          nilattr("starargs")))))))))),
-                    setstr("ctx","Ld"))))))),
-                setstr("arg","body"))))))),
-            nilattr("kwargs"),
-            setattr("args", attribute_args_type_list_39 (alist_0())),
-            setstr("func","Module"),
-            nilattr("starargs")))))))))))))))
+                              setrec(value, record_type_a_list_31(arecord(
+                                setrec(a_list, record_type_ctx_elts_30(arecord(
+                                  setattr(elts, attribute_elts_type_list_29 (alist_0())),
+                                  setstr(ctx,Ld))))))),
+                              setstr(arg,decorator_list))))))),
+                          nilattr(kwargs),
+                          setattr(args, attribute_args_type_list_33 (alist_0())),
+                          setstr(func,FunctionDef),
+                          nilattr(starargs)))))))))),
+                    setstr(ctx,Ld))))))),
+                setstr(arg,body))))))),
+            nilattr(kwargs),
+            setattr(args, attribute_args_type_list_39 (alist_0())),
+            setstr(func,Module),
+            nilattr(starargs)))))))))))))))
